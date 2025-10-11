0267c3ea16c4883bc7331eadcf0d6af5f95f51ad
rizespbya <rizespb@ya.ru>
2025-10-11 18:10:49 +0300

    Задания из Основы Python
    Задания к главе 6.3 "Модуль requests" хендбука Основы Python

---
+++ Python/yandex-python-handbook/chapter_6_3.py
@@ -0,0 +1,135 @@
+"""Задания к главе 6.3.
+
+Модуль requests
+
+Хендбук Яндекс "Основы Python".
+"""
+
+# A
+
+# +
+import sys
+
+from requests import delete, get, post, put
+
+base_host = "http://127.0.0.1:5000"
+
+response = get(base_host)
+
+message = response.content.decode("utf-8")
+
+print(message)
+# -
+
+# B
+
+# +
+base_host = f"http://{input()}"
+
+result = 0
+
+while response_b := int(get(base_host).text):
+    result += response_b
+
+print(result)
+# -
+
+# C
+
+# +
+base_host = f"http://{input()}"
+
+response_c = get(base_host).json()
+
+sum_c = 0
+for item in response_c:
+    sum_c += item if isinstance(item, int) else 0
+
+print(sum_c)
+# -
+
+# D
+
+# +
+base_host = f"http://{input()}"
+key = input()
+
+response_d = get(base_host).json()
+
+print(response_d.get(key, "No data"))
+# -
+
+# E
+
+# +
+host, *paths = map(str.strip, sys.stdin)
+
+results = [get(f"http://{host}{path}").json() for path in paths]
+
+print(sum(sum(nums) for nums in results))
+# -
+
+# F
+
+# +
+host = f"http://{input()}"
+
+users = get(f"{host}/users").json()
+
+names = list(
+    map(lambda user: f"{user['last_name']} {user['first_name']}", users)
+)
+
+print("\n".join(sorted(names)))
+# -
+
+# G
+
+# +
+host = f"http://{input()}"
+user_id = input()
+
+text = "".join(string for string in sys.stdin)
+
+
+try:
+    user = get(f"{host}/users/{user_id}").json()
+except ValueError:
+    print("Пользователь не найден")
+else:
+    for key in user:
+        text = text.replace(f"{{{key}}}", str(user[key]))
+    print(text)
+# -
+
+# H
+
+# +
+keys = ["username", "last_name", "first_name", "email"]
+
+host, *data = map(str.strip, sys.stdin)
+
+user_dict: dict[str, str] = dict(zip(keys, data))
+
+post(f"http://{host}/users/", json=user_dict)
+# -
+
+# I
+
+# +
+host, user_id, *updated_data = map(str.strip, sys.stdin)
+
+updated_data_dict: dict[str, str] = dict(
+    map(lambda item: item.split("="), updated_data)
+)
+
+print(updated_data_dict)
+put(f"http://{host}/users/{user_id}", json=updated_data_dict)
+# -
+
+# J
+
+# +
+host, user_id = map(str.strip, sys.stdin)
+
+delete(f"http://{host}/users/{user_id}")
